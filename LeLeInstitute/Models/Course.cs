56b4061095264ba849d16388c8a7898c904c5fe9
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-28 21:17:31 +0530

    Instructor

--- LeLeInstitute/Models/Course.cs
+++ LeLeInstitute/Models/Course.cs
@@ -10,17 +10,13 @@ namespace LeLeInstitute.Models
 {
     public class Course
     {
-
         public int Id { get; set; }
         [Display(Name = "Course Title")]
         [Required(ErrorMessage = "Please fill the course Title")]
         public string CourseName { get; set; }
         public int Credits { get; set; }
-
-
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
-
         public ICollection<Enrollment> Enrollments { get; set; }
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
--- LeLeInstitute/Models/CourseAssignment.cs
+++ LeLeInstitute/Models/CourseAssignment.cs
@@ -7,11 +7,8 @@ namespace LeLeInstitute.Models
 {
     public class CourseAssignment
     {
-
         public int Id { get; set; }
         public Instructor Instructor { get; set; }
-
-
         public int CourseId { get; set; }
         public Course Course { get; set; }
     }
